6114259799f919a6e6c9dd4f844f1f4da186e7cd
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-08-30 07:19:13 -0700

    Se agrega forma basica para el programa 2

--- src/pages/unidad1/components/Programa2Unidad1/Programa2Unidad1.jsx
+++ src/pages/unidad1/components/Programa2Unidad1/Programa2Unidad1.jsx
@@ -1,13 +1,58 @@
 function Programa2Unidad2() {
   return (
     <div>
-      <h2>Programa 2 - Unidad 1</h2>
+      <hr className="border-black" />
+      <h2 className="text-xl font-bold">Programa 2 - Unidad 1</h2>
       <div>
-        <h3>Instrucciones</h3>
+        <h3 className="font-bold">Instrucciones</h3>
         <p>
           Crear un programa que acepte una expresion logica cualquiera con las
-          variables P y Q, y que genere su tabla de verdad.
+          variables <span className="font-bold">P</span> y{" "}
+          <span className="font-bold">Q</span>, y que genere su tabla de verdad.
         </p>
+      </div>
+      <div className="my-5">
+        <input
+          type="text"
+          placeholder="Introduzca su expresion logica"
+          className="mx-10 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        <div className="flex items-center justify-center flex-col">
+          <h4 className="font-bold mt-5">Tabla de verdad</h4>
+          <div class="overflow-x-auto relative rounded-xl">
+            <table class="w-full text-sm text-left text-black">
+              <thead class="text-m text-gray-700 uppercase bg-gray-50 dark:bg-gray-70">
+                <tr>
+                  <th class="py-3 px-6">P</th>
+                  <th class="py-3 px-6">Q</th>
+                  <th class="py-3 px-6">Expresion Logica</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="bg-white border-b ">
+                  <th class="py-4 px-6 ">0</th>
+                  <td class="py-4 px-6">0</td>
+                  <td class="py-4 px-6">0</td>
+                </tr>
+                <tr class="bg-white border-b ">
+                  <th class="py-4 px-6 ">0</th>
+                  <td class="py-4 px-6">0</td>
+                  <td class="py-4 px-6">0</td>
+                </tr>
+                <tr class="bg-white ">
+                  <th class="py-4 px-6 ">0</th>
+                  <td class="py-4 px-6">0</td>
+                  <td class="py-4 px-6">0</td>
+                </tr>
+                <tr class="bg-white ">
+                  <th class="py-4 px-6 ">0</th>
+                  <td class="py-4 px-6">0</td>
+                  <td class="py-4 px-6">0</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
