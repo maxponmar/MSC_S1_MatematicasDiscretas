97e12bebbc57499e9c836a7f0d004834a350e596
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-08-30 07:17:25 -0700

    se agrega linea de separacion.

--- src/pages/unidad1/components/Programa1Unidad1/Programa1Unidad1.jsx
+++ src/pages/unidad1/components/Programa1Unidad1/Programa1Unidad1.jsx
@@ -13,56 +13,60 @@ function Programa1Unidad1() {
   }, [numero1, numero2]);
 
   return (
-    <div className="flex items-center justify-center flex-col">
-      <div className="flex items-center justify-center text-center mx-20 text-xl">
-        <div className="w-1/2">
-          <h2>Instrucción:</h2>
-          <p className="text-left">
-            Desarrollar un programa que compare dos números enteros y diga cuál
-            es el mayor. Presente el Algoritmo o Pseudocódigo, diagrama de
-            flujo, código y ejecutable (Pueden utilizar la herramienta PSeInt).
-          </p>
+    <>
+      <hr className="border-black" />
+      <div className="flex items-center justify-center flex-col">
+        <div className="flex items-center justify-center text-center mx-20 text-xl">
+          <div className="w-1/2">
+            <h2>Instrucción:</h2>
+            <p className="text-left">
+              Desarrollar un programa que compare dos números enteros y diga
+              cuál es el mayor. Presente el Algoritmo o Pseudocódigo, diagrama
+              de flujo, código y ejecutable (Pueden utilizar la herramienta
+              PSeInt).
+            </p>
+          </div>
+          <div className="w-1/2">
+            <div>
+              <label className="mr-2">Número 1</label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="number"
+                defaultValue={numero1}
+                onChange={(e) => {
+                  setNumero1(+e.target.value);
+                }}
+                step={1}
+              />
+            </div>
+            <div>
+              <label className="mr-2">Número 1</label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="number"
+                defaultValue={numero2}
+                onChange={(e) => {
+                  setNumero2(+e.target.value);
+                }}
+                step={1}
+              />
+            </div>
+            <div>
+              <h3>Resulltado</h3>
+              <p>{resultado}</p>
+            </div>
+          </div>
         </div>
-        <div className="w-1/2">
-          <div>
-            <label className="mr-2">Número 1</label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="number"
-              defaultValue={numero1}
-              onChange={(e) => {
-                setNumero1(+e.target.value);
-              }}
-              step={1}
-            />
-          </div>
-          <div>
-            <label className="mr-2">Número 1</label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="number"
-              defaultValue={numero2}
-              onChange={(e) => {
-                setNumero2(+e.target.value);
-              }}
-              step={1}
-            />
-          </div>
-          <div>
-            <h3>Resulltado</h3>
-            <p>{resultado}</p>
-          </div>
+        <div>
+          <h3 className="font-bold text-xl">Pseudocódigo</h3>
+          <img src={pseudocodigo} alt="Pseudocódigo del Algoritmo" />
+        </div>
+        <div className="mb-10">
+          <h3 className="font-bold text-xl">Diagrama</h3>
+          <img src={diagrama} alt="Diagrama del algoritmo" />
         </div>
       </div>
-      <div>
-        <h3 className="font-bold text-xl">Pseudocódigo</h3>
-        <img src={pseudocodigo} alt="Pseudocódigo del Algoritmo" />
-      </div>
-      <div className="mb-10">
-        <h3 className="font-bold text-xl">Diagrama</h3>
-        <img src={diagrama} alt="Diagrama del algoritmo" />
-      </div>
-    </div>
+    </>
   );
 }
 
