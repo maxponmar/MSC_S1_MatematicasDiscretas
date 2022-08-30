38fcd1f4a9ecb47895518bbef5e19068ed830d3f
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-08-30 06:58:48 -0700

    Se cambia estilo para items de sidebar.

--- src/components/sidebar/SidebarLink.jsx
+++ src/components/sidebar/SidebarLink.jsx
@@ -7,7 +7,7 @@ function SidebarLink({ title, link, href, onClick }) {
     sidebarItem = (
       <Link
         to={link ? link : "/"}
-        className=" w-full h-12 flex items-center justify-end pr-5 py-5 my-1 ease-in-out duration-300 hover:bg-continental hover:text-white cursor-pointer font-bold text-xl text-right"
+        className=" w-full h-12 flex items-center justify-end pr-5 py-5 my-1 ease-in-out duration-300 hover:bg-itn hover:text-white cursor-pointer font-bold text-xl text-right"
         onClick={onClick}
       >
         {title}
