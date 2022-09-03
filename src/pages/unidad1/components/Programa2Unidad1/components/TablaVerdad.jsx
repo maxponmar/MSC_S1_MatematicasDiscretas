8fb3fdd57ab6cd023e74e1b0c17e47379fa86475
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-09-03 11:24:39 -0700

    Se actualiza interfaz para generar tabla de verdad.

--- src/pages/unidad1/components/Programa2Unidad1/components/TablaVerdad.jsx
+++ src/pages/unidad1/components/Programa2Unidad1/components/TablaVerdad.jsx
@@ -1,41 +1,11 @@
 import React from "react";
 
-export default function TablaVerdad() {
+export default function TablaVerdad({ stringTable }) {
   return (
-    <div className="flex items-center justify-center flex-col">
+    <div className="flex items-center justify-center flex-col w-full">
       <h4 className="font-bold mt-5">Tabla de verdad</h4>
-      <div className="overflow-x-auto relative rounded-xl">
-        <table className="w-full text-sm text-left text-black">
-          <thead className="text-m text-gray-700 uppercase bg-gray-50 dark:bg-gray-70">
-            <tr>
-              <th className="py-3 px-6">P</th>
-              <th className="py-3 px-6">Q</th>
-              <th className="py-3 px-6">Expresion Logica</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="bg-white border-b ">
-              <th className="py-4 px-6 ">0</th>
-              <td className="py-4 px-6">0</td>
-              <td className="py-4 px-6">0</td>
-            </tr>
-            <tr className="bg-white border-b ">
-              <th className="py-4 px-6 ">0</th>
-              <td className="py-4 px-6">0</td>
-              <td className="py-4 px-6">0</td>
-            </tr>
-            <tr className="bg-white ">
-              <th className="py-4 px-6 ">0</th>
-              <td className="py-4 px-6">0</td>
-              <td className="py-4 px-6">0</td>
-            </tr>
-            <tr className="bg-white ">
-              <th className="py-4 px-6 ">0</th>
-              <td className="py-4 px-6">0</td>
-              <td className="py-4 px-6">0</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="overflow-x-auto relative rounded-xl w-full">
+        <textarea className="w-full h-full" value={stringTable} />
       </div>
     </div>
   );
