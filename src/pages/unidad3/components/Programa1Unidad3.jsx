dc2612ff111564003bdd93afa8f72c4a5bbd9208
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-10-10 21:27:46 -0700

    Se arreglan validaciones.

--- src/pages/unidad3/components/Programa1Unidad3.jsx
+++ src/pages/unidad3/components/Programa1Unidad3.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
+import Modal from "../../../components/modal/Modal";
 import toast from "react-hot-toast";
+import DiagramaFlujo from "../assets/DiagramasFlujoFactorial.drawio.png";
 
 function Programa1Unidad3() {
-  const [entradaParaIterativo, setEntradaParaIterativo] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+
+  const [entradaParaIterativo, setEntradaParaIterativo] = useState(1);
   const [resultadoParaIterativo, setResultadoParaIterativo] = useState(0);
-  const [entradaParaRecursivo, setEntradaParaRecursivo] = useState(0);
+  const [entradaParaRecursivo, setEntradaParaRecursivo] = useState(1);
   const [resultadoParaRecursivo, setResultadoParaRecursivo] = useState(0);
+
+  const [esValidoIterativo, setesValidoIterativo] = useState(true);
+  const [esValidoRecursivo, setesValidoRecursivo] = useState(true);
 
   return (
     <div>
@@ -22,12 +29,15 @@ function Programa1Unidad3() {
         <p>Realizar diagrama de fluja para cada método.</p>
       </div>
       <div>
-        <div className="flex items-center justify-center p-3 gap-3">
+        <div className="flex flex-col items-center justify-center p-3 gap-3 sm:flex-row">
           <h3 className="font-bold text-xl">Iterativa</h3>
           <form
             onSubmit={(e) => {
               e.preventDefault();
-
+              if (!esValidoIterativo) {
+                toast.error("Entrada no valida por favor verifique.");
+                return;
+              }
               // Calculo Iterativo
               let resultado = 1;
               for (let i = entradaParaIterativo; i > 0; i--) {
@@ -42,13 +52,15 @@ function Programa1Unidad3() {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="username"
               placeholder="Entrada para Iterativa"
-              defaultValue={0}
+              defaultValue={1}
               onChange={(e) => {
                 const number = Number(e.target.value);
-                if (!Number.isInteger(number)) {
+                if (!Number.isInteger(number) || number < 0) {
                   toast.error("Por favor, solo números enteros.");
+                  setesValidoIterativo(false);
                   return;
                 }
+                setesValidoIterativo(true);
                 setEntradaParaIterativo(number);
               }}
             />
@@ -64,15 +76,18 @@ function Programa1Unidad3() {
             <p>{resultadoParaIterativo}</p>
           </div>
         </div>
-        <div className="flex items-center justify-center p-3 gap-3">
+        <div className="flex flex-col items-center justify-center p-3 gap-3 sm:flex-row">
           <h3 className="font-bold text-xl">Rescursiva</h3>
           <form
             onSubmit={(e) => {
               e.preventDefault();
-
+              if (!esValidoRecursivo) {
+                toast.error("Entrada no valida por favor verifique.");
+                return;
+              }
               // Calculo Rescursivo
               const factorialRecursivo = (n) => {
-                if (n == 1) return 1;
+                if (n == 1 || n == 0) return 1;
                 else return n * factorialRecursivo(n - 1);
               };
 
@@ -86,13 +101,15 @@ function Programa1Unidad3() {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="username"
               placeholder="Entrada para Iterativa"
-              defaultValue={0}
+              defaultValue={1}
               onChange={(e) => {
                 const number = Number(e.target.value);
-                if (!Number.isInteger(number)) {
-                  toast.error("Por favor, solo números enteros.");
+                if (!Number.isInteger(number) || number < 0) {
+                  toast.error("Por favor, solo números enteros positivos.");
+                  setesValidoRecursivo(false);
                   return;
                 }
+                setesValidoRecursivo(true);
                 setEntradaParaRecursivo(number);
               }}
             />
@@ -109,6 +126,20 @@ function Programa1Unidad3() {
           </div>
         </div>
       </div>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        type="submit"
+        onClick={() => setShowModal(true)}
+      >
+        Mostrar Diagrama
+      </button>
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        title="Diagramas de Flujo"
+      >
+        <img src={DiagramaFlujo} alt="Diagramas de Flujo" />
+      </Modal>
     </div>
   );
 }
